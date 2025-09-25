f23e7a907d6277432fd9a42fa598f04e6f2f11c3
andreamanini98 <79704110+andreamanini98@users.noreply.github.com>
2025-09-25 10:38:24 +0200

    Added the ability to parse multiple timed automata and timed arenas from a directory.

--- TARZAN/headers/library.h
+++ TARZAN/headers/library.h
@@ -18,11 +18,29 @@ timed_automaton::ast::timedAutomaton parseTimedAutomaton(std::string const &path
 
 
 /**
+ * @brief Parses all Timed Automata saved as .txt files from a folder.
+ *
+ * @param folderPath the path in which files to parse are located.
+ * @return a std::vector containing the parsed Timed Automata.
+ */
+std::vector<timed_automaton::ast::timedAutomaton> parseTimedAutomataFromFolder(std::string const &folderPath);
+
+
+/**
  * @brief Parses a Timed Arena from a .txt file.
  *
  * @param path the path in which the file to parse is located.
  * @return a timed_automaton::ast::timedArena representation of the Timed Arena.
  */
 timed_automaton::ast::timedArena parseTimedArena(std::string const &path);
+
+
+/**
+ * @brief Parses all Timed Arenas saved as .txt files from a folder.
+ *
+ * @param folderPath the path in which files to parse are located.
+ * @return a std::vector containing the parsed Timed Arenas.
+ */
+std::vector<timed_automaton::ast::timedArena> parseTimedArenasFromFolder(std::string const &folderPath);
 
 #endif //TARZAN_LIBRARY_H
