2f33c94cfddde58cf7875657b7832bdc5c9328b2
andreamanini98 <79704110+andreamanini98@users.noreply.github.com>
2025-10-28 15:26:55 +0100

    Completed maler benchmark.

---
+++ TARZAN/benchmark_executables/maler.cpp
@@ -0,0 +1,43 @@
+#include "TARZAN/parser/ast.h"
+#include "TARZAN/headers/library.h"
+#include "TARZAN/regions/Region.h"
+#include "TARZAN/utilities/file_utilities.h"
+#include "TARZAN/regions/networkOfTA/RTSNetwork.h"
+
+
+/**
+ * @param path the path to the directory containing all benchmark subdirectories.
+ */
+inline void testMaler(const std::string &path)
+{
+    const std::vector<timed_automaton::ast::timedAutomaton> automata = parseTimedAutomataFromFolder(path);
+    const networkOfTA::RTSNetwork net(automata);
+
+    const auto &locationsToInt = net.getLocationsToInt();
+
+    std::vector<std::optional<int>> goal(4);
+    goal[0] = locationsToInt[0].at("End1");
+    goal[1] = locationsToInt[1].at("End2");
+    goal[2] = locationsToInt[2].at("End3");
+    goal[3] = locationsToInt[3].at("End4");
+
+    const auto res = net.forwardReachability(goal, DFS);
+}
+
+
+int main(const int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        // Key parameter automatically inserted by scripts but ignored here.
+        std::cerr << "Usage: " << argv[0] << " <path> <key>" << std::endl;
+        return 1;
+    }
+
+    const std::string path = argv[1];
+
+    // Query: E<> (job1.End1 && job2.End2 && job3.End3 && job4.End4)
+    testMaler(path);
+
+    return 0;
+}
--- TARZAN/benchmark_scripts/run_all.sh
+++ TARZAN/benchmark_scripts/run_all.sh
@@ -2,7 +2,7 @@
 
 EXECUTABLES_PATH="../../executables/benchmark_executables"
 BENCHMARKS_PATH="../benchmarks"
-TOTAL_RUNS=1
+TOTAL_RUNS=100
 OUTPUT_PATH="../../output/benchmark_results"
 TIMEOUT=1
 
@@ -107,16 +107,27 @@ TIMEOUT=1
 
 # lynch
 
-subdirs=()
-while IFS= read -r dir; do
-    subdirs+=("$dir")
-done < <(find "${BENCHMARKS_PATH}/lynch/liana" -mindepth 1 -maxdepth 1 -type d | sort)
+## subdirs=()
+## while IFS= read -r dir; do
+##     subdirs+=("$dir")
+## done < <(find "${BENCHMARKS_PATH}/lynch/liana" -mindepth 1 -maxdepth 1 -type d | sort)
+##
+## num_dirs=${#subdirs[@]}
+##
+## for ((key=0; key<num_dirs; key++)); do
+##     current_dir="${subdirs[$key]}"
+##     folder_name=$(basename "$current_dir")
+##
+##     ./sh_network_ta.sh "${EXECUTABLES_PATH}/lynch" "${current_dir}" "${TOTAL_RUNS}" "${OUTPUT_PATH}/lynch" "${folder_name}" "${TIMEOUT}" "${key}"
+## done
 
-num_dirs=${#subdirs[@]}
 
+# ---
+
+
+# maler
+
+num_dirs=$(find "${BENCHMARKS_PATH}/maler/liana" -mindepth 1 -maxdepth 1 -type d | wc -l)
 for ((key=0; key<num_dirs; key++)); do
-    current_dir="${subdirs[$key]}"
-    folder_name=$(basename "$current_dir")
-
-    ./sh_network_ta.sh "${EXECUTABLES_PATH}/lynch" "${current_dir}" "${TOTAL_RUNS}" "${OUTPUT_PATH}/lynch" "${folder_name}" "${TIMEOUT}" "${key}"
+  ./sh_network_ta.sh "${EXECUTABLES_PATH}/maler" "${BENCHMARKS_PATH}/maler/liana" "${TOTAL_RUNS}" "${OUTPUT_PATH}/maler" "maler" "${TIMEOUT}" "${key}"
 done
