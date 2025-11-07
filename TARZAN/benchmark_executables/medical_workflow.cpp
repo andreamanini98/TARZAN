4a0fbdbf3dec405e8e64d8d3d70f5f93202600e9
andreamanini98 <79704110+andreamanini98@users.noreply.github.com>
2025-11-07 16:14:29 +0100

    Completed medical_workflow liana benchmark.

---
+++ TARZAN/benchmark_executables/medical_workflow.cpp
@@ -0,0 +1,45 @@
+#include "TARZAN/parser/ast.h"
+#include "TARZAN/headers/library.h"
+#include "TARZAN/regions/Region.h"
+#include "TARZAN/utilities/file_utilities.h"
+#include "TARZAN/regions/networkOfTA/RTSNetwork.h"
+
+
+/**
+ * @param path the path to the directory containing all benchmark subdirectories.
+ * @param benchmarkKey a string used to retrieve the necessary auxiliary data for the benchmark at hand.
+ */
+inline void testMedicalWorkflow(const std::string &path, const std::string &benchmarkKey)
+{
+    const std::vector<timed_automaton::ast::timedAutomaton> automata = parseTimedAutomataFromFolder(path);
+    const networkOfTA::RTSNetwork net(automata);
+
+    const auto &locationsToInt = net.getLocationsToInt();
+
+    const int numPatients = std::stoi(benchmarkKey) * 10 + 10;
+    const int numElements = numPatients + 2;
+
+    auto goal = std::vector<std::optional<int>>(numElements, std::nullopt);
+    for (int i = 2; i < numElements; i++)
+        goal[i] = locationsToInt[i].at("Done");
+
+    const auto res = net.forwardReachability(goal, DFS);
+}
+
+
+int main(const int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        std::cerr << "Usage: " << argv[0] << " <path> <key>" << std::endl;
+        return 1;
+    }
+
+    const std::string path = argv[1];
+    const std::string benchmarkKey = argv[2];
+
+    // Query: E<> ( every patient done )
+    testMedicalWorkflow(path, benchmarkKey);
+
+    return 0;
+}
