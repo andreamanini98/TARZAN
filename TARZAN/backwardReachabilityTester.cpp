9398036bbdf4d9e625d0f1cf1a21f1b984f2c4a7
andreamanini98 <79704110+andreamanini98@users.noreply.github.com>
2025-11-25 10:55:53 +0100

    Added flower backward reachability scalability tests.

--- TARZAN/backwardReachabilityTester.cpp
+++ TARZAN/backwardReachabilityTester.cpp
@@ -655,6 +655,238 @@ void testFischerFlat6()
 }
 
 
+inline void testFlowerBackward2()
+{
+    std::cout << "Test Flower Backward 2: showing that from the region reached forward it is possible to go back to an initial region.\n\n";
+
+    const std::string path = "/Users/echo/Desktop/PhD/Tools/TARZAN/TARZAN/benchmarks/flower/liana/flower_02/";
+
+    const std::string automatonFileName = "Flower.txt";
+    const timed_automaton::ast::timedAutomaton automaton = parseTimedAutomaton(path + automatonFileName);
+
+    const region::RTS regionTransitionSystem(automaton);
+
+    std::cout << "Forward computation output:" << std::endl;
+
+    const std::vector<region::Region> rts = regionTransitionSystem.forwardReachability(0, DFS);
+
+    std::cout << '\n';
+
+    std::cout << "Starting from region:\n" << rts[0].toString() << std::endl;
+
+    std::cout << "Backward computation output:" << std::endl;
+
+    const std::vector<region::Region> predecessors = regionTransitionSystem.backwardReachability(rts, DFS);
+
+    std::cout << "Predecessors contents:\n";
+    for (const auto &region: predecessors)
+        std::cout << region.toString() << std::endl;
+}
+
+
+inline void testFlowerBackward4()
+{
+    std::cout << "Test Flower Backward 4: showing that from the region reached forward it is possible to go back to an initial region.\n\n";
+
+    const std::string path = "/Users/echo/Desktop/PhD/Tools/TARZAN/TARZAN/benchmarks/flower/liana/flower_04/";
+
+    const std::string automatonFileName = "Flower.txt";
+    const timed_automaton::ast::timedAutomaton automaton = parseTimedAutomaton(path + automatonFileName);
+
+    const region::RTS regionTransitionSystem(automaton);
+
+    std::cout << "Forward computation output:" << std::endl;
+
+    const std::vector<region::Region> rts = regionTransitionSystem.forwardReachability(0, DFS);
+
+    std::cout << '\n';
+
+    std::cout << "Starting from region:\n" << rts[0].toString() << std::endl;
+
+    std::cout << "Backward computation output:" << std::endl;
+
+    const std::vector<region::Region> predecessors = regionTransitionSystem.backwardReachability(rts, DFS);
+
+    std::cout << "Predecessors contents:\n";
+    for (const auto &region: predecessors)
+        std::cout << region.toString() << std::endl;
+}
+
+
+inline void testFlowerBackward6()
+{
+    std::cout << "Test Flower Backward 6: showing that from the region reached forward it is possible to go back to an initial region.\n\n";
+
+    const std::string path = "/Users/echo/Desktop/PhD/Tools/TARZAN/TARZAN/benchmarks/flower/liana/flower_06/";
+
+    const std::string automatonFileName = "Flower.txt";
+    const timed_automaton::ast::timedAutomaton automaton = parseTimedAutomaton(path + automatonFileName);
+
+    const region::RTS regionTransitionSystem(automaton);
+
+    std::cout << "Forward computation output:" << std::endl;
+
+    const std::vector<region::Region> rts = regionTransitionSystem.forwardReachability(0, DFS);
+
+    std::cout << '\n';
+
+    std::cout << "Starting from region:\n" << rts[0].toString() << std::endl;
+
+    std::cout << "Backward computation output:" << std::endl;
+
+    const std::vector<region::Region> predecessors = regionTransitionSystem.backwardReachability(rts, DFS);
+
+    std::cout << "Predecessors contents:\n";
+    for (const auto &region: predecessors)
+        std::cout << region.toString() << std::endl;
+}
+
+
+inline void testFlowerBackward8()
+{
+    std::cout << "Test Flower Backward 8: showing that from the region reached forward it is possible to go back to an initial region.\n\n";
+
+    const std::string path = "/Users/echo/Desktop/PhD/Tools/TARZAN/TARZAN/benchmarks/flower/liana/flower_08/";
+
+    const std::string automatonFileName = "Flower.txt";
+    const timed_automaton::ast::timedAutomaton automaton = parseTimedAutomaton(path + automatonFileName);
+
+    const region::RTS regionTransitionSystem(automaton);
+
+    std::cout << "Forward computation output:" << std::endl;
+
+    const std::vector<region::Region> rts = regionTransitionSystem.forwardReachability(0, DFS);
+
+    std::cout << '\n';
+
+    std::cout << "Starting from region:\n" << rts[0].toString() << std::endl;
+
+    std::cout << "Backward computation output:" << std::endl;
+
+    const std::vector<region::Region> predecessors = regionTransitionSystem.backwardReachability(rts, DFS);
+
+    std::cout << "Predecessors contents:\n";
+    for (const auto &region: predecessors)
+        std::cout << region.toString() << std::endl;
+}
+
+
+inline void testFlowerBackward10()
+{
+    std::cout << "Test Flower Backward 10: showing that from the region reached forward it is possible to go back to an initial region.\n\n";
+
+    const std::string path = "/Users/echo/Desktop/PhD/Tools/TARZAN/TARZAN/benchmarks/flower/liana/flower_10/";
+
+    const std::string automatonFileName = "Flower.txt";
+    const timed_automaton::ast::timedAutomaton automaton = parseTimedAutomaton(path + automatonFileName);
+
+    const region::RTS regionTransitionSystem(automaton);
+
+    std::cout << "Forward computation output:" << std::endl;
+
+    const std::vector<region::Region> rts = regionTransitionSystem.forwardReachability(0, DFS);
+
+    std::cout << '\n';
+
+    std::cout << "Starting from region:\n" << rts[0].toString() << std::endl;
+
+    std::cout << "Backward computation output:" << std::endl;
+
+    const std::vector<region::Region> predecessors = regionTransitionSystem.backwardReachability(rts, DFS);
+
+    std::cout << "Predecessors contents:\n";
+    for (const auto &region: predecessors)
+        std::cout << region.toString() << std::endl;
+}
+
+
+inline void testFlowerBackward12()
+{
+    std::cout << "Test Flower Backward 12: showing that from the region reached forward it is possible to go back to an initial region.\n\n";
+
+    const std::string path = "/Users/echo/Desktop/PhD/Tools/TARZAN/TARZAN/benchmarks/flower/liana/flower_12/";
+
+    const std::string automatonFileName = "Flower.txt";
+    const timed_automaton::ast::timedAutomaton automaton = parseTimedAutomaton(path + automatonFileName);
+
+    const region::RTS regionTransitionSystem(automaton);
+
+    std::cout << "Forward computation output:" << std::endl;
+
+    const std::vector<region::Region> rts = regionTransitionSystem.forwardReachability(0, DFS);
+
+    std::cout << '\n';
+
+    std::cout << "Starting from region:\n" << rts[0].toString() << std::endl;
+
+    std::cout << "Backward computation output:" << std::endl;
+
+    const std::vector<region::Region> predecessors = regionTransitionSystem.backwardReachability(rts, DFS);
+
+    std::cout << "Predecessors contents:\n";
+    for (const auto &region: predecessors)
+        std::cout << region.toString() << std::endl;
+}
+
+
+inline void testFlowerBackward14()
+{
+    std::cout << "Test Flower Backward 14: showing that from the region reached forward it is possible to go back to an initial region.\n\n";
+
+    const std::string path = "/Users/echo/Desktop/PhD/Tools/TARZAN/TARZAN/benchmarks/flower/liana/flower_14/";
+
+    const std::string automatonFileName = "Flower.txt";
+    const timed_automaton::ast::timedAutomaton automaton = parseTimedAutomaton(path + automatonFileName);
+
+    const region::RTS regionTransitionSystem(automaton);
+
+    std::cout << "Forward computation output:" << std::endl;
+
+    const std::vector<region::Region> rts = regionTransitionSystem.forwardReachability(0, DFS);
+
+    std::cout << '\n';
+
+    std::cout << "Starting from region:\n" << rts[0].toString() << std::endl;
+
+    std::cout << "Backward computation output:" << std::endl;
+
+    const std::vector<region::Region> predecessors = regionTransitionSystem.backwardReachability(rts, DFS);
+
+    std::cout << "Predecessors contents:\n";
+    for (const auto &region: predecessors)
+        std::cout << region.toString() << std::endl;
+}
+
+
+inline void testFlowerBackward16()
+{
+    std::cout << "Test Flower Backward 16: showing that from the region reached forward it is possible to go back to an initial region.\n\n";
+
+    const std::string path = "/Users/echo/Desktop/PhD/Tools/TARZAN/TARZAN/benchmarks/flower/liana/flower_16/";
+
+    const std::string automatonFileName = "Flower.txt";
+    const timed_automaton::ast::timedAutomaton automaton = parseTimedAutomaton(path + automatonFileName);
+
+    const region::RTS regionTransitionSystem(automaton);
+
+    std::cout << "Forward computation output:" << std::endl;
+
+    const std::vector<region::Region> rts = regionTransitionSystem.forwardReachability(0, DFS);
+
+    std::cout << '\n';
+
+    std::cout << "Starting from region:\n" << rts[0].toString() << std::endl;
+
+    std::cout << "Backward computation output:" << std::endl;
+
+    const std::vector<region::Region> predecessors = regionTransitionSystem.backwardReachability(rts, DFS);
+
+    std::cout << "Predecessors contents:\n";
+    for (const auto &region: predecessors)
+        std::cout << region.toString() << std::endl;
+}
+
+
 int main()
 {
     //std::cout << "\n--------------------\n\n";
@@ -689,15 +921,24 @@ int main()
     //
     //test7();
 
-    testFischerFlat2();
+    //testFischerFlat2();
+    //
+    //testFischerFlat3();
+    //
+    //testFischerFlat4();
+    //
+    //testFischerFlat5();
+    //
+    //testFischerFlat6();
 
-    testFischerFlat3();
-
-    testFischerFlat4();
-
-    testFischerFlat5();
-
-    testFischerFlat6();
+    testFlowerBackward2();
+    testFlowerBackward4();
+    testFlowerBackward6();
+    testFlowerBackward8();
+    testFlowerBackward10();
+    testFlowerBackward12();
+    testFlowerBackward14();
+    testFlowerBackward16();
 
     return 0;
 }
