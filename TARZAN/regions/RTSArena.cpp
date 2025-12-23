20320472f13643d5dae3d9040e0cb1ef2b95ce06
andreamanini98 <79704110+andreamanini98@users.noreply.github.com>
2025-12-23 09:52:21 +0100

    Small fixes.

--- TARZAN/regions/RTSArena.cpp
+++ TARZAN/regions/RTSArena.cpp
@@ -650,7 +650,8 @@ bool region::RTSArena::timedSafety(regionSet &setG, std::vector<RegionPtr> &toPr
         const size_t oldSetGSize = setG.size();
 
         omegaFilter(setG, toProcess, filteredRegionsOmega, {}, false, true);
-        deltaFilter(setG, toProcess, filteredRegionsDelta, {}, false, true);
+        // We pass filteredRegionsOmega to deltaFilter, since we want to keep the alternating semantics.
+        deltaFilter(filteredRegionsOmega, toProcess, filteredRegionsDelta, {}, false, true);
 
         // Computing the intersection between regions returned by omega and delta filters and setG.
         filteredRegionsOmega.merge(filteredRegionsDelta);
