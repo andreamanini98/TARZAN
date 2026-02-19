c224f95e5cf283001136ebac1504eb1a0f945989
andreamanini98 <79704110+andreamanini98@users.noreply.github.com>
2026-02-19 13:30:44 +0100

    Added new executable pc_andX_win.cpp

---
+++ TARZAN/benchmarks_games/games_executables/pc_andX_win.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#include "TARZAN/parser/ast.h"
+#include "TARZAN/headers/library.h"
+#include "TARZAN/regions/RTSArena.h"
+
+
+int main()
+{
+    // std::cout << "\nSolving a conjunction of NEXT formulae. Should be winning for the controller." << std::endl;
+
+    const std::string arenaPath = "../../TARZAN/benchmarks_games/models/production_cell/arena/production_cell.txt";
+    const timed_automaton::ast::timedArena arena = TARZAN::parseTimedArena(arenaPath);
+
+    const std::string formulaPath = "../../TARZAN/benchmarks_games/models/production_cell/winning_conditions/and_next.txt";
+    const cltloc::ast::conjunctionOfFormulae phi = TARZAN::parseConjunctionOfFormulae(formulaPath);
+
+    // std::cout << phi.to_string() << std::endl;
+    // std::cout << rts.to_string() << std::endl;
+
+    if (const region::RTSArena rts(arena, phi); rts.solveTimedCLTLocGame(phi))
+        std::cout << "-----" << std::endl;
+    else
+        std::cout << "----- " << std::endl;
+
+    return 0;
+}
