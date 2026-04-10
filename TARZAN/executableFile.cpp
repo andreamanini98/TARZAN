ea75d8cfb7b8ef02a34cb69ef602394d5c9ef016
andreamanini98 <79704110+andreamanini98@users.noreply.github.com>
2026-04-10 15:40:21 +0200

    Refactoring.

--- TARZAN/executableFile.cpp
+++ TARZAN/executableFile.cpp
@@ -10,7 +10,7 @@ int main()
     const std::string arenaPath = "/Users/echo/Desktop/PhD/Tools/TARZAN/TARZAN/benchmarks_games/models/production_cell/arena/production_cell.txt";
     const timed_automaton::ast::timedArena arena = TARZAN::parseTimedArena(arenaPath);
 
-    const std::string formulaPath = "/Users/echo/Desktop/PhD/Tools/TARZAN/TARZAN/benchmarks_games/models/production_cell/winning_conditions/safety0.txt";
+    const std::string formulaPath = "/Users/echo/Desktop/PhD/Tools/TARZAN/TARZAN/benchmarks_games/models/production_cell/winning_conditions/until6.txt";
     const cltloc::ast::generalCLTLocFormula phi = TARZAN::parseGeneralCLTLocFormula(formulaPath);
 
     region::RTSArena rts(arena, phi, true);
