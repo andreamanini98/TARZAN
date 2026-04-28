737377ae1491aaaecee8315f605239fe931c2b0a
andreamanini98 <79704110+andreamanini98@users.noreply.github.com>
2026-04-28 15:45:03 +0200

    Enhanced medapp example.

--- TARZAN/executableFile.cpp
+++ TARZAN/executableFile.cpp
@@ -7,9 +7,9 @@
 
 int main()
 {
-    // constexpr std::string formulaName = "reachability0.txt";
+     constexpr std::string formulaName = "reachability0.txt";
     // constexpr std::string formulaName = "reachability1.txt";
-    constexpr std::string formulaName = "and_next0.txt";
+//     constexpr std::string formulaName = "and_next0.txt";
 
     const std::string formulaPath = "/Users/echo/Desktop/PhD/Tools/TARZAN/TARZAN/benchmarks_games/models/med_app/winning_conditions/" + formulaName;
 
@@ -18,8 +18,8 @@ int main()
     const std::string arenaPath ="/Users/echo/Desktop/PhD/Tools/TARZAN/TARZAN/benchmarks_games/models/med_app/arena/" + arenaName;
     const timed_automaton::ast::timedArena arena = TARZAN::parseTimedArena(arenaPath);
 
-    // const cltloc::ast::generalCLTLocFormula phi = TARZAN::parseGeneralCLTLocFormula(formulaPath);
-    const cltloc::ast::conjunctionOfFormulae phi = TARZAN::parseConjunctionOfFormulae(formulaPath);
+     const cltloc::ast::generalCLTLocFormula phi = TARZAN::parseGeneralCLTLocFormula(formulaPath);
+    // const cltloc::ast::conjunctionOfFormulae phi = TARZAN::parseConjunctionOfFormulae(formulaPath);
 
     region::RTSArena rts(arena, phi, true);
 
