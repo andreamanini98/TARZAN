5d401d9a916c86345ccd8f25bf51d1947875c748
andreamanini98 <79704110+andreamanini98@users.noreply.github.com>
2026-05-05 09:50:11 +0200

    Small name changes in medapp arena.

--- TARZAN/executableFile.cpp
+++ TARZAN/executableFile.cpp
@@ -10,14 +10,17 @@ int main()
     // constexpr std::string formulaName = "reachability0.txt"; // Tbot can complete a single patient procedure.
     // constexpr std::string formulaName = "reachability1.txt"; // Tbot can complete a single patient procedure in at most 50 time units.
     // constexpr std::string formulaName = "safety0.txt";       // Tbot always makes the drug expire (not proper safety).
-    constexpr std::string formulaName = "safety1.txt";          // Tbot can always complete a patient procedure without raising interrupts or making the drug expire.
+    // constexpr std::string formulaName = "safety1.txt";          // Tbot can always complete a patient procedure without raising interrupts or making the drug expire.
 
     // constexpr std::string formulaName = "and_next0.txt"; // Tbot can complete a single patient procedure by calling the nurse during pulse measurement.
+    constexpr std::string formulaName = "and_next1.txt"; // Tbot can complete a single patient procedure by raising an interrupt during pulse measurement
+    //                                                      three times, after which a nurse is called. The nurse intervention makes Tbot complete without
+    //                                                      raising interrupts or requiring safety mode.
 
     const std::string formulaPath = "/Users/echo/Desktop/PhD/Tools/TARZAN/TARZAN/benchmarks_games/models/med_app/winning_conditions/" + formulaName;
 
-    const cltloc::ast::generalCLTLocFormula phi = TARZAN::parseGeneralCLTLocFormula(formulaPath);
-    // const cltloc::ast::conjunctionOfFormulae phi = TARZAN::parseConjunctionOfFormulae(formulaPath);
+    // const cltloc::ast::generalCLTLocFormula phi = TARZAN::parseGeneralCLTLocFormula(formulaPath);
+    const cltloc::ast::conjunctionOfFormulae phi = TARZAN::parseConjunctionOfFormulae(formulaPath);
 
     // ---
 
