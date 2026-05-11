ec0601040242ec14f7eafa06dd6ca9ba440257a6
andreamanini98 <79704110+andreamanini98@users.noreply.github.com>
2026-05-11 11:31:36 +0200

    Swapped the role of nurse and doctor in the model medapp.

--- TARZAN/executableFile.cpp
+++ TARZAN/executableFile.cpp
@@ -7,24 +7,24 @@
 
 int main()
 {
-    constexpr std::string formulaName = "reachability0.txt"; // Tbot can complete a single patient procedure.
+    // constexpr std::string formulaName = "reachability0.txt"; // Tbot can complete a single patient procedure.
     // constexpr std::string formulaName = "reachability1.txt"; // Tbot can complete a single patient procedure in at most 50 time units.
 
     // constexpr std::string formulaName = "safety0.txt";       // Tbot always makes the drug expire (not proper safety).
     // constexpr std::string formulaName = "safety1.txt";       // Tbot can always complete a patient procedure without raising interrupts or making the drug expire.
 
-    // constexpr std::string formulaName = "and_next0.txt"; // Tbot can complete a single patient procedure by calling the nurse during pulse measurement.
+    // constexpr std::string formulaName = "and_next0.txt"; // Tbot can complete a single patient procedure by calling the doctor during pulse measurement.
     // constexpr std::string formulaName = "and_next1.txt"; // Tbot can complete a single patient procedure by raising an interrupt during pulse measurement
-    //                                                      three times, after which a nurse is called. The nurse intervention makes Tbot complete without
+    //                                                      three times, after which a doctor is called. The doctor intervention makes Tbot complete without
     //                                                      raising interrupts or requiring safety mode.
-    //constexpr std::string formulaName = "and_next2.txt"; // As above, but after the nurse is called and by completing the mission in under 50 time units.
+    constexpr std::string formulaName = "and_next2.txt"; // As above, but after the doctor is called and by completing the mission in under 50 time units.
 
     // constexpr std::string formulaName = "q_int_unreachable.txt";
 
     const std::string formulaPath = "/Users/echo/Desktop/PhD/Tools/TARZAN/TARZAN/benchmarks_games/models/med_app/winning_conditions/" + formulaName;
 
-    const cltloc::ast::generalCLTLocFormula phi = TARZAN::parseGeneralCLTLocFormula(formulaPath);
-    //const cltloc::ast::conjunctionOfFormulae phi = TARZAN::parseConjunctionOfFormulae(formulaPath);
+    // const cltloc::ast::generalCLTLocFormula phi = TARZAN::parseGeneralCLTLocFormula(formulaPath);
+    const cltloc::ast::conjunctionOfFormulae phi = TARZAN::parseConjunctionOfFormulae(formulaPath);
 
     // ---
 
